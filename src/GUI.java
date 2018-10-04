a76421fc5e2c848835738a32a21fa5e8484705e6
Nicklas <s185105@student.dtu.dk>
2018-10-04 17:21:12 +0200

    Added some descriptions, and builded the core for the game, but it's throwing a NullPointerException

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,8 @@
+//Importing different packages.
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
 /**
  * This is the class where I build the GUI for the game.
  *
@@ -9,19 +14,72 @@ public class GUI
     //Starting out with importing my other classes
     public Clicker click;
 
+    //This is where I import different things like: JFrame, JButton and etc.
+    private JFrame frame;
+    private JLabel clicks;
+
+    //Here I'm making my variables
+    public boolean frameRunning = false;
+
     /**
      * This is running everything.
      */
     public static void main (String[] args)
     {
-
+        new GUI();
     }
 
     /**
-     * This is where I build the GUI.
+     * This is where I make the frame, and all the other important things.
      */
     public GUI()
     {
-
+        new Clicker();
+        makeFrame();
     }
+
+    /**
+     * This is where I update the different labels.
+     */
+    private void updateLabel()
+    {
+        while(frameRunning) {
+            clicks.setText(Integer.toString(click.playerClick));
+        }
+    }
+
+    /**
+     * This is where I build the GUI (Frame).
+     */
+    public void makeFrame()
+    {
+        frameRunning = true;
+        frame = new JFrame("Clicker Game");
+        JPanel contentPane = (JPanel)frame.getContentPane();
+        contentPane.setBorder(new EmptyBorder(1, 60, 1, 60));
+
+        contentPane.setLayout(new BorderLayout(12, 12));
+
+        JPanel testPanel = new JPanel();
+        testPanel.setLayout(new GridLayout(2, 1));
+
+        clicks = new JLabel("0");
+        testPanel.add(clicks);
+
+        JButton clickerButton = new JButton("Player Click");
+        clickerButton.addActionListener(e -> {click.clicks();});
+        testPanel.add(clickerButton);
+
+        contentPane.add(testPanel, BorderLayout.CENTER);
+
+        frame.pack();
+
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(d.width/2 - frame.getWidth()/2, d.height/2 - frame.getHeight()/2);
+        frame.setVisible(true);
+
+        updateLabel();
+    }
+
+
 }
