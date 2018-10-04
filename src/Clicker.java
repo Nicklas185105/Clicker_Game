3007c0e1ea51b2b1a8482dbbfbb781dd4839dca1
Nicklas <s185105@student.dtu.dk>
2018-10-04 16:12:23 +0200

    Commiting everything I have so far of the project

---
+++ src/Clicker.java
@@ -0,0 +1,13 @@
+/**
+ * This is where I make the core of the game.
+ *
+ * @author Nicklas Lydersen
+ * @version 1.0.1
+ */
+public class Clicker
+{
+    public Clicker()
+    {
+
+    }
+}
---
+++ src/GUI.java
@@ -0,0 +1,21 @@
+/**
+ * This is the class where I build the GUI for the game.
+ *
+ * @author Nicklas Lydersen
+ * @version 1.0.1
+ */
+public class GUI
+{
+    //Starting out with importing my other classes
+    public Clicker click;
+
+    public static void main (String[] args)
+    {
+
+    }
+
+    public GUI()
+    {
+
+    }
+}
